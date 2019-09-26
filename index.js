d0784269190e929db7b2c0fe18a4b057160da49c
Cristina Nastasa <cnastasa@salesforce.com>
2019-09-25 17:00:10 -0700

    Update URL and returned JSON to mimic a response from a shipping service

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-function someJson() {
-    return { a: 'mytest' };
+function shippingRatesJson() {
+    return {
+        status: 'calculated',
+        rate: {
+            serviceName: 'Canada Post',
+            serviceCode: 'SNC9600',
+            shipmentCost: 11.99,
+            otherCost: 5.99
+        }
+    };
 }
 
 express()
@@ -11,5 +19,5 @@ express()
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .get('/json1', (req, res) => res.json(someJson()))
+    .get('/calculate-shipping-rates', (req, res) => res.json(shippingRatesJson()))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
