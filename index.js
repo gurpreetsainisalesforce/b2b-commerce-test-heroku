15ad5022f3875f7c3efd57b52ad7c208a1913b96
Gurpreet Saini <gurpreet.saini@salesforce.com>
2022-03-07 16:13:54 -0800

    Taxation Support
    Support Gross and Net Taxation with Adjustments

--- index.js
+++ index.js
@@ -83,6 +83,96 @@ function getTaxRates(amountsBySKU) {
     return json;
 }
 
+// Return the same list of SKUs with the calculated tax amount and the tax rate 0.08 for each SKU
+function getTaxRates(amountsBySKU, taxType) {
+    let json = {};
+    const skusArray = amountsBySKU.slice(1, -1).split(',');
+    for (const skuAndAmount of skusArray) {
+        const sku = skuAndAmount.split(':')[0];
+        const amount = skuAndAmount.split(':')[1];
+        const taxRate = 0.08;
+        var finalTaxAmount = 0.00;
+        if(taxType == 'Gross') {
+            finalTaxAmount = amount * taxRate / (1 + taxRate);
+        } else {
+            finalTaxAmount = amount * taxRate;
+        }
+        json[sku.slice(1, -1)] = {
+            taxAmount: finalTaxAmount,
+            taxRate: taxRate,
+            taxName: 'GST'
+        };
+    }
+    return json;
+}
+
+// Return the same list of SKUs with the calculated tax amount and the tax rate 0.08 for each SKU
+function getTaxRatesWithAdjustments(amountsBySKU, country, state, taxType) {
+    var taxRate = 0.08;
+    let json = {};
+    const jsonObject = JSON.parse(amountsBySKU);
+    for (var key in jsonObject) {
+        const cartItemId = jsonObject[key].cartItemId;
+        const sku = jsonObject[key].sku;
+        const amount = jsonObject[key].amount;
+        const tierAdjustment = jsonObject[key].tierAdj;
+        const itemizedPromotions = jsonObject[key].itemizedPromos;
+        const quantity = jsonObject[key].quantity;
+
+        const noSalesTaxUSStates = ['AK', 'DE', 'MT', 'NH', 'OR'];
+    
+        if(country == 'US' && noSalesTaxUSStates.includes(state)) {
+            taxRate = 0.00;
+        }
+
+        if(country == 'JP') {
+            taxRate = 0.15;
+        }
+
+        var itemizedPromotionTax = 0.00;
+        var itemizedPromotionTaxArr = [];
+        var netUnitPrice = 0.00;
+        var grossUnitPrice = 0.00;
+
+        var multiplier = 0.00;
+
+        if(taxType == 'Gross') {
+            multiplier = taxRate / (1 + taxRate)
+        } else {
+            multiplier = taxRate;
+        }
+
+        var cartItemTax = amount * multiplier;
+        var tierAdjustmentTax = (tierAdjustment ? tierAdjustment : 0.00) * multiplier;
+        itemizedPromotions.forEach(element => {
+            var itemTaxAmount = (element.amount ? element.amount : 0.00) * multiplier;
+            itemizedPromotionTaxArr.push({id: element.id, taxAmount: itemTaxAmount});
+            itemizedPromotionTax = itemizedPromotionTax + itemTaxAmount;
+        });
+
+        if(taxType == 'Gross') {
+            grossUnitPrice = amount / quantity;
+            netUnitPrice = (amount - cartItemTax) / quantity;
+        } else {
+            grossUnitPrice = (amount + cartItemTax) / quantity;
+            netUnitPrice = amount / quantity;
+        }
+
+        json[sku] = {
+            cartItemId: cartItemId,
+            taxAmount: cartItemTax,
+            adjustmentTaxAmount: tierAdjustmentTax,
+            itemizedPromotionTaxAmounts: itemizedPromotionTaxArr,
+            totalItemizedPromotionTaxAmount: itemizedPromotionTax,
+            grossUnitPrice: grossUnitPrice,
+            netUnitPrice: netUnitPrice,
+            taxRate: taxRate,
+            taxName: 'GST'
+        };
+    }
+    return json;
+}
+
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
@@ -93,4 +183,6 @@ express()
     .get('/get-inventory', (req, res) => res.json(getInventory(req.query.skus)))
     .get('/get-sales-prices', (req, res) => res.json(getSalesPrices(req.query.skus)))
     .get('/get-tax-rates', (req, res) => res.json(getTaxRates(req.query.amountsBySKU)))
+    .get('/get-tax-rates-by-tax-type', (req, res) => res.json(getTaxRates(req.query.amountsBySKU, req.query.taxType)))
+    .get('/get-tax-rates-with-adjustments', (req, res) => res.json(getTaxRatesWithAdjustments(req.query.amountsBySKU, req.query.country, req.query.state, req.query.taxType)))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
