3101aba2818a8d75ec719b23dfe736205d196309
Gurpreet Saini <gurpreet.saini@salesforce.com>
2022-07-08 10:26:24 -0700

    update: refactor based on changed request payload and increase request size for large cart support

--- index.js
+++ index.js
@@ -250,11 +250,76 @@ function getTaxRatesWithAdjustments(amountsBySKU, country, state, taxType) {
     return json;
 }
 
+function getTaxRatesWithAdjustmentsPost(amountsBySKU, country, state, taxType) {
+    var taxRate = 0.15;
+    let json = {};
+    for (var key in amountsBySKU) {
+        const cartItemId = amountsBySKU[key].Id;
+        const amount = amountsBySKU[key].TotalLineAmount;
+        const tierAdjustment = amountsBySKU[key].AdjustmentAmount;
+        let itemizedPromotions = [];
+        if(amountsBySKU[key].CartItemPriceAdjustments) {
+            itemizedPromotions = amountsBySKU[key].CartItemPriceAdjustments.records;
+        }
+        const quantity = amountsBySKU[key].Quantity;
+    
+        if(country == 'US') {
+            taxRate = 0.08;
+            const noSalesTaxUSStates = ['AK', 'DE', 'MT', 'NH', 'OR'];
+            if (noSalesTaxUSStates.includes(state)) {
+                taxRate = 0.00;
+            }
+        }
+
+        var itemizedPromotionTax = 0.00;
+        var itemizedPromotionTaxArr = [];
+        var netUnitPrice = 0.00;
+        var grossUnitPrice = 0.00;
+
+        var multiplier = 0.00;
+
+        if(taxType == 'Gross') {
+            multiplier = taxRate / (1 + taxRate)
+        } else {
+            multiplier = taxRate;
+        }
+
+        var cartItemTax = amount * multiplier;
+        var tierAdjustmentTax = (tierAdjustment ? tierAdjustment : 0.00) * multiplier;
+        itemizedPromotions.forEach(element => {
+            var itemTaxAmount = (element.TotalAmount ? element.TotalAmount : 0.00) * multiplier;
+            itemizedPromotionTaxArr.push({id: element.Id, taxAmount: itemTaxAmount});
+            itemizedPromotionTax = itemizedPromotionTax + itemTaxAmount;
+        });
+
+        if(taxType == 'Gross') {
+            grossUnitPrice = amount / quantity;
+            netUnitPrice = (amount - cartItemTax) / quantity;
+        } else {
+            grossUnitPrice = (amount + cartItemTax) / quantity;
+            netUnitPrice = amount / quantity;
+        }
+
+        json[cartItemId] = {
+            cartItemId: cartItemId,
+            taxAmount: cartItemTax,
+            adjustmentTaxAmount: tierAdjustmentTax,
+            itemizedPromotionTaxAmounts: itemizedPromotionTaxArr,
+            totalItemizedPromotionTaxAmount: itemizedPromotionTax,
+            grossUnitPrice: grossUnitPrice,
+            netUnitPrice: netUnitPrice,
+            taxRate: taxRate,
+            taxName: 'GST'
+        };
+    }
+    return json;
+}
+
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .use(express.json())
+    .use(express.json({limit: '5mb'}))
     .get('/', (req, res) => res.render('pages/index'))
     .get('/calculate-shipping-rates', (req, res) => res.json(shippingRatesJson()))
     .get('/calculate-shipping-rates-with-lang', (req, res) => res.json(shippingRatesJson(req.query.lang)))
@@ -265,5 +330,5 @@ express()
     .get('/get-tax-rates', (req, res) => res.json(getTaxRates(req.query.amountsBySKU)))
     .get('/get-tax-rates-by-tax-type', (req, res) => res.json(getTaxRatesByTaxType(req.query.amountsBySKU, req.query.taxType)))
     .get('/get-tax-rates-with-adjustments', (req, res) => res.json(getTaxRatesWithAdjustments(JSON.parse(req.query.amountsBySKU), req.query.country, req.query.state, req.query.taxType)))
-    .post('/get-tax-rates-with-adjustments', (req, res) => res.json(getTaxRatesWithAdjustments(req.body.amountsBySKU, req.body.country, req.body.state, req.body.taxType)))
+    .post('/get-tax-rates-with-adjustments-post', (req, res) => res.json(getTaxRatesWithAdjustmentsPost(req.body.amountsBySKU, req.body.country, req.body.state, req.body.taxType)))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
